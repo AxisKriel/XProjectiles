4f02cca9f81e194e5405bee831a6846f01aadedd
Enerdy <rodrigo.rente@live.com>
2016-10-31 23:44:04 +0000

    Initial commit

---
+++ Projectiles/Commands.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using TShockAPI;
+using Terraria;
+using Terraria.ID;
+using NDesk.Options;
+
+namespace Projectiles
+{
+	public partial class Plugin
+	{
+		void RegisterCommands()
+		{
+			Commands.ChatCommands.Add(new Command("projectiles.spawn", doProj, "xproj", "projectiles")
+			{
+				AllowServer = false,
+				HelpText = "Commands for spawning projectiles."
+			});
+		}
+
+		void doProj(CommandArgs e)
+		{
+			// Defaults
+			int type = ProjectileID.JestersArrow;
+			float x = e.Player.X + 1f;
+			float y = e.Player.Y;
+			float speedX = 8f;
+			float speedY = 0f;
+			int damage = 1;
+
+			OptionSet o = new OptionSet
+			{
+				{ "t|type=", v => Int32.TryParse(v, out type) },
+				{ "x|posX=", v => Single.TryParse(v, out x) },
+				{ "y|posY=", v => Single.TryParse(v, out y) },
+				{ "X|speedX=", v => Single.TryParse(v, out speedX) },
+				{ "Y|speedY=", v => Single.TryParse(v, out speedY) },
+				{ "d|dmg|damage=", v => Int32.TryParse(v, out damage) }
+			};
+
+			// Parse parameters
+			o.Parse(e.Parameters);
+
+			if (type < 0 || type >= ProjectileID.Count)
+			{
+				e.Player.SendErrorMessage("Invalid projectile ID!");
+				return;
+			}
+
+			int proj = Projectile.NewProjectile(x, y, speedX, speedY, type, damage, 0f);
+			e.Player.SendInfoMessage($"[{proj}] Spawned {type} @ ({x},{y}).");
+		}
+	}
+}
+
---
+++ Projectiles/Plugin.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using TShockAPI;
+using TerrariaApi.Server;
+using Terraria;
+
+namespace Projectiles
+{
+	/* All we need is:
+		1. Spawn projectiles relative to player's location
+		2. Alter projectile velocity/direction
+		3. Alter projectile damage
+	*/
+
+	[ApiVersion(1, 25)]
+	public partial class Plugin : TerrariaPlugin
+	{
+		public override string Author
+		{
+			get { return "Enerdy"; }
+		}
+
+		public override string Name
+		{
+			get { return "Projectiles"; }
+		}
+
+		public override Version Version
+		{
+			get { return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version; }
+		}
+
+		public Plugin(Main game) : base(game)
+		{
+
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				ServerApi.Hooks.GameInitialize.Deregister(this, onInitialize);
+			}
+		}
+
+		public override void Initialize()
+		{
+			ServerApi.Hooks.GameInitialize.Register(this, onInitialize);
+		}
+
+		void onInitialize(EventArgs e)
+		{
+			RegisterCommands();
+		}
+	}
+}
+
