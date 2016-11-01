1017616b36ad1bdda93ead53d681857b87b41d2a
Enerdy <rodrigo.rente@live.com>
2016-11-01 21:27:19 +0000

    Allow spawning of multiple projectiles at once

--- Projectiles/Commands.cs
+++ Projectiles/Commands.cs
@@ -21,11 +21,12 @@ namespace Projectiles
 		{
 			// Defaults
 			int type = ProjectileID.JestersArrow;
-			float x = e.Player.X + 1f;
+			float x = e.Player.X;
 			float y = e.Player.Y;
 			float speedX = 8f * e.Player.TPlayer.direction;
 			float speedY = 0f;
 			int damage = 1;
+			uint count = 1;
 
 			OptionSet o = new OptionSet
 			{
@@ -34,7 +35,8 @@ namespace Projectiles
 				{ "y|posY=", v => Single.TryParse(v, out y) },
 				{ "X|speedX=", v => Single.TryParse(v, out speedX) },
 				{ "Y|speedY=", v => Single.TryParse(v, out speedY) },
-				{ "d|dmg|damage=", v => Int32.TryParse(v, out damage) }
+				{ "d|dmg|damage=", v => Int32.TryParse(v, out damage) },
+				{ "c|count=", v => UInt32.TryParse(v, out count) }
 			};
 
 			// Parse parameters
@@ -46,8 +48,33 @@ namespace Projectiles
 				return;
 			}
 
-			int proj = Projectile.NewProjectile(x, y, speedX, speedY, type, damage, 0f);
-			e.Player.SendInfoMessage($"[{proj}] Spawned {type} @ ({x},{y}).");
+			if (count < 1 || count > Main.maxProjectiles)
+				e.Player.SendErrorMessage("Invalid projectile count!");
+			else if (count == 1)
+			{
+				int proj = Projectile.NewProjectile(x, y, speedX, speedY, type, damage, 0f);
+				e.Player.SendInfoMessage($"[{proj}] Spawned {type} @ ({x},{y}).");
+			}
+			else
+			{
+				uint total = count;
+
+				// Initial projectile
+				Projectile.NewProjectile(x, y, speedX, speedY, type, damage, 0f);
+
+				// Spawn further projectiles above and below the initial point, alternating
+				for (int i = 32; count > 0; i = i + 32)
+				{
+					Projectile.NewProjectile(x, y - i, speedX, speedY, type, damage, 0f);
+					count--;
+					if (count > 0)
+					{
+						Projectile.NewProjectile(x, y + i, speedX, speedY, type, damage, 0f);
+						count--;
+					}
+				}
+				e.Player.SendInfoMessage($"Spawned {total} projectiles of type {type}.");
+			}
 		}
 	}
 }
